f11541f6bf690619c186ad4a8d276788213140ce
Evgeniy Galkovskiy <zhegal@gmail.com>
2024-03-19 20:19:43 +0200

    Adding icons

---
+++ sw.js
@@ -0,0 +1,8 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-window.prod.mjs'
+);
+
+// workbox.routing.registerModule(
+//   ({ request }) => request.destination === 'image',
+//   new workbox.strategies.CacheFirst()
+// );
