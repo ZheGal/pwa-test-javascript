7eed7439fd413fb0b70755bd3c3e9fb22cbc3c63
Evgeniy Galkovskiy <zhegal@gmail.com>
2024-03-19 20:23:59 +0200

    hello world

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-window.prod.mjs'
 );
 
-// workbox.routing.registerModule(
-//   ({ request }) => request.destination === 'image',
-//   new workbox.strategies.CacheFirst()
-// );
+workbox.routing.registerModule(
+  ({ request }) => request.destination === 'image',
+  new workbox.strategies.NetworkFirst()
+);
